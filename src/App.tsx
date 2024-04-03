5889c27618b356742d3b8c41f2c86b6a1198d703
Babatunde-Segun <officiallyvincify@gmail.com>
2024-04-03 23:26:10 +0100

    adding todo todo.ts  as a class

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [
+    new Todo("Learning on wednesday"),
+    new Todo("Learning type on wednesday"),
+  ];
   return (
     <div>
-      <Todos items={["Learning TypeScript", "Learning React"]} />
+      <Todos items={todos} />
     </div>
   );
 }
